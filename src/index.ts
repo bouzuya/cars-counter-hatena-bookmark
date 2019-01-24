9b2b2bcf613e69fcce5bbb90af40b6eb073e12a0
bouzuya <m@bouzuya.net>
2019-01-24 12:47:52 +0900

    js -> ts

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+
+interface Counts { [k: string]: number; }
+
+export default function main(): Promise<Counts> {
+  const url = 'http://b.hatena.ne.jp/bouzuya/';
+  return fetch(url)
+    .then((response) => {
+      const status = response.status;
+      if (status !== 200) throw new Error('invalid status code : ' + status);
+      return response.text();
+    })
+    .then((html) => {
+      const $ = cheerio.load(html);
+      const labelAndCounts = [] as Array<{ label: string; count: string; }>;
+      $('#profile-count-navi dl').each((_, e) => {
+        const label = $(e).find('dt').text();
+        const count = $(e).find('dd').text().replace(/,/g, '');
+        labelAndCounts.push({ label, count });
+      });
+
+      // tslint:disable:object-literal-key-quotes
+      const labelAndKeys: Array<{ key: string; label: string; }> = [
+        { label: 'お気に入られ', key: 'Hatena Bookmark Follower' },
+        { label: 'お気に入り', key: 'Hatena Bookmark Follow' },
+        { label: 'ブックマーク', key: 'Hatena Bookmark' }
+      ];
+      // tslint:enable
+
+      const counts = labelAndCounts.reduce((a, { label, count }) => {
+        const entry = labelAndKeys.find(({ label: l }) => l === label);
+        return typeof entry === 'undefined'
+          ? a
+          : { ...a, [entry.key]: count };
+      }, {});
+
+      return counts;
+    });
+}
